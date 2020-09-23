df964b8f1bd7bc446a8e35c29ee66d17e0adb59d
Danila Artemov <gor.ros@yandex.ru>
2020-09-24 00:13:55 +0400

    Создал Laravel проект. Сделал макет.

---
+++ deanery/resources/views/home.blade.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body class="vh-100">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent border-bottom rounded">
+        <a class="navbar-brand" href="#">Деканат</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample09">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Главная<span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-md-0">
+                <input class="form-control" type="text" placeholder="Введите запрос" aria-label="Search">
+            </form>
+        </div>
+    </nav>
+    <div class="container-fluid h-100 m-0">
+        <div class="row h-100">
+            <div class="col-4 border-right p-0">
+                <div class="col border-bottom mt-3">
+                    <h5>Разделы</h5>
+                </div>
+                <ul class="list-group list-group-flush m-2">
+                    <li class="list-group-item user-select-none">ФЭиВТ</li>
+                    <li class="list-group-item user-select-none">Программная инженерия</li>
+                    <li class="list-group-item user-select-none">4 курс</li>
+                    <li class="list-group-item user-select-none">ПрИн-466</li>
+                </ul>
+            </div>
+            <div class="col p-0">
+                <div class="col border-bottom mt-3">
+                    <h5>Выберите нужный пункт</h5>
+                </div>
+                <div class="row m-3">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФАСТиВ
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФАТ
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФТКМ
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-3">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФТПП
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФЭиУ
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ФЭиВТ
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-3">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-body">
+                                ХТФ
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
